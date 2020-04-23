7a6d9ac531c6d5a242e883368bcccc3868d7a61a
Robert Szymczak <robbie@Roberts-MacBook-Pro.local>
2020-04-23 18:21:31 -0400

    f

--- js/secondNav.js
+++ js/secondNav.js
@@ -1,6 +1,9 @@
 const secondDonateLink = document.getElementById('second-donate-link');
 const secondAboutLink = document.getElementById('second-about-link');
 const secondContactLink = document.getElementById('second-contact-link');
+const indexGalleryLink = document.getElementById('index-gallery-link');
+const indexHistoryLink = document.getElementById('index-history-link');
+const indexBoardLink = document.getElementById('index-board-link');
 
 secondDonateLink.addEventListener('click', (e) => {
   location = "./main.html#donate-start";
@@ -10,4 +13,10 @@ secondAboutLink.addEventListener('click', (e) => {
 });
 secondContactLink.addEventListener('click', (e) => {
   location = "./main.html#contact-start";
+});
+indexHistoryLink.addEventListener('click', (e) => {
+  location = "./second.html#history-start";
+});
+indexBoardLink.addEventListener('click', (e) => {
+  location = "./second.html#board-start";
 });
